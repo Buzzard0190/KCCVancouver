f677b26a3c80ed7b33012a5288209f8470283d21
Christopher Hight <christopher.hight@hp.com>
2018-03-19 11:05:40 -0700

    Added in some base material for week 4 class

---
+++ Class/Week 4/Chips Challenge Assets/ChipsChallenge.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="ChipsChallenge" tilewidth="32" tileheight="32" tilecount="208" columns="13">
+ <image source="Color_Tileset.gif" width="416" height="512"/>
+</tileset>
